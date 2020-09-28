b80fa1f02d8a0933ba6d1dc124fb008cfe34dcf5
shilibrad <42542976+shilibrad@users.noreply.github.com>
2020-09-29 01:58:05 +1000

    reducers for shop and cart

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import { createStructuredSelector } from "reselect";
+import "./collections-overview.styles.scss";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateProps)(CollectionsOverview);
